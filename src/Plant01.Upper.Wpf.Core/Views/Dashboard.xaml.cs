f20ea48313fd5c1d6574c544979605aa033c337a
Stephen Shi <stephenxshi@outlook.com>
2025-11-28 15:16:11 +0800

    独立一层Plant01.Upper.Wpf.Core，以后UI的业务逻辑都放这里

---
+++ src/Plant01.Upper.Wpf.Core/Views/Dashboard.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace Plant01.Upper.Wpf.Views;
+
+/// <summary>
+/// Interaction logic for Dashboard.xaml
+/// </summary>
+public partial class Dashboard : UserControl
+{
+    public Dashboard()
+    {
+        InitializeComponent();
+    }
+}
